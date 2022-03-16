9d1434febb846ab3d078bb0e0ba1fca9e90143b5
DimitrisMitroulis <amfimitrul@gmail.com>
2022-03-16 14:10:03 +0200

    small changes all over the code

--- app/src/main/java/com/example/flickrbrowser/GetFlickrJsonData.java
+++ app/src/main/java/com/example/flickrbrowser/GetFlickrJsonData.java
@@ -10,119 +10,114 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-
 class GetFlickrJsonData extends AsyncTask<String, Void, List<Photo>> implements GetRawData.OnDownloadComplete {
-    //String containing query , Void because we won't implement progress bar, We will be returning list of photos
     private static final String TAG = "GetFlickrJsonData";
 
-    @Override//requires to be implemented with async task
-    protected List<Photo> doInBackground(String... params) {
-        Log.d(TAG, "doInBackground: starts");
-        String destinationUri = createUri(params[0], mLanguage, mMatchAll);
-        
-        GetRawData getRawData = new GetRawData(this);
-        getRawData.runInSameThread(destinationUri);//not using execute so as not to run in a new thread
-        Log.d(TAG, "doInBackground: ends");
-        return mPhotoList;
-    }
-
-    @Override
-    protected void onPostExecute(List<Photo> photos) {
-        //called after onDownloadComplete
-        // we need the mcallBack here as well cause onDownloadComplete is running on a different thread
-        Log.d(TAG, "onPostExecute: starts");
-        if(mCallBack != null ){
-            mCallBack.onDataAvailable(mPhotoList, DownloadStatus.OK);
-        }
-        Log.d(TAG, "onPostExecute: ends");
-    }
-
-    @Override
-    public void onDownloadComplete(String data, DownloadStatus status) {
-        Log.d(TAG, "onDownloadComplete: Status = " + status);
-
-        if (status == DownloadStatus.OK) {
-            mPhotoList = new ArrayList<>();
-            try {
-                JSONObject jsonData = new JSONObject(data);
-                JSONArray itemsArray = jsonData.getJSONArray("items");
-
-                for (int i = 0; i < itemsArray.length(); i++) {
-                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
-                    String title = jsonPhoto.getString("title");
-                    String author = jsonPhoto.getString("author");
-                    String authorid = jsonPhoto.getString("author_id");
-                    String tags = jsonPhoto.getString("tags");
-                    String description = jsonPhoto.getString("description");
-                    String date = jsonData.getString("date_taken");
-
-                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
-                    String photoUrl = jsonMedia.getString("m");
-
-                    String link = photoUrl.replaceFirst("_m.", "_b.");//change the photo url to change it's size to b(big)
-
-                    Photo photoObject = new Photo(title, author, authorid, link, tags, photoUrl, date, description);
-                    mPhotoList.add(photoObject);
-
-                    Log.d(TAG, "onDownloadComplete: " + photoObject.toString());
-
-                }
-
-            } catch (JSONException jsone) {
-                jsone.printStackTrace();
-
-                Log.d(TAG, "onDownloadComplete: Exception" + jsone.getMessage());
-                status = DownloadStatus.FAILED_OR_EMPTY;
-            }
-        }
-
-        if (mCallBack != null) {
-            ///no inform the caller that processing is done- possibly returning null if there
-            //was an error
-            mCallBack.onDataAvailable(mPhotoList, status);
-        }
-    }
-
     private List<Photo> mPhotoList = null;
-    private String mBaseUrl;
+    private String mBaseURL;
     private String mLanguage;
     private boolean mMatchAll;
 
     private final OnDataAvailable mCallBack;
+    private boolean runningOnSameThread = false;
 
     interface OnDataAvailable {
         void onDataAvailable(List<Photo> data, DownloadStatus status);
-
     }
 
-    public GetFlickrJsonData(OnDataAvailable callBack, String language, String baseUrl, boolean matchAll) {
+    public GetFlickrJsonData(OnDataAvailable callBack, String baseURL, String language, boolean matchAll) {
         Log.d(TAG, "GetFlickrJsonData called");
-        mBaseUrl = baseUrl;
+        mBaseURL = baseURL;
+        mCallBack = callBack;
         mLanguage = language;
         mMatchAll = matchAll;
-        mCallBack = callBack;
     }
 
     void executeOnSameThread(String searchCriteria) {
-        Log.d(TAG, "excecuteOnSameThread: starts ");
-        String destinationUrl = createUri(searchCriteria, mLanguage, mMatchAll);
+        Log.d(TAG, "executeOnSameThread starts");
+        runningOnSameThread = true;
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
 
         GetRawData getRawData = new GetRawData(this);
-        getRawData.execute(destinationUrl);
-        Log.d(TAG, "excecuteOnSameThread: ends");
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread ends");
+    }
+
+    @Override
+    protected void onPostExecute(List<Photo> photos) {
+        Log.d(TAG, "onPostExecute starts");
+
+        if(mCallBack != null) {
+            mCallBack.onDataAvailable(mPhotoList, DownloadStatus.OK);
+        }
+        Log.d(TAG, "onPostExecute ends");
+    }
+
+    @Override
+    protected List<Photo> doInBackground(String... params) {
+        Log.d(TAG, "doInBackground starts");
+        String destinationUri = createUri(params[0], mLanguage, mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.runInSameThread(destinationUri);
+        Log.d(TAG, "doInBackground ends");
+        return mPhotoList;
     }
 
     private String createUri(String searchCriteria, String lang, boolean matchAll) {
-        Log.d(TAG, "createUri: starts");
+        Log.d(TAG, "createUri starts");
 
-        return Uri.parse(mBaseUrl).buildUpon()
+        return Uri.parse(mBaseURL).buildUpon()
                 .appendQueryParameter("tags", searchCriteria)
                 .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
                 .appendQueryParameter("lang", lang)
-                .appendQueryParameter("format", lang)
+                .appendQueryParameter("format", "json")
                 .appendQueryParameter("nojsoncallback", "1")
                 .build().toString();
-
     }
 
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete starts. Status = " + status);
+
+        if(status == DownloadStatus.OK) {
+            mPhotoList = new ArrayList<>();
+
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+
+                for(int i=0; i<itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authorId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
+
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+
+                    Photo photoObject = new Photo(title, author, authorId, link, tags, photoUrl);
+                    mPhotoList.add(photoObject);
+
+                    Log.d(TAG, "onDownloadComplete " + photoObject.toString());
+                }
+            } catch(JSONException jsone) {
+                jsone.printStackTrace();
+                Log.e(TAG, "onDownloadComplete: Error processing Json data " + jsone.getMessage());
+                status = DownloadStatus.FAILED_OR_EMPTY;
+            }
+        }
+
+        if(runningOnSameThread && mCallBack != null) {
+            // now inform the caller that processing is done - possibly returning null if there
+            // was an error
+            mCallBack.onDataAvailable(mPhotoList, status);
+        }
+
+        Log.d(TAG, "onDownloadComplete ends");
+    }
 }
+
--- app/src/main/java/com/example/flickrbrowser/Photo.java
+++ app/src/main/java/com/example/flickrbrowser/Photo.java
@@ -1,61 +1,46 @@
 package com.example.flickrbrowser;
 
 class Photo {
-    //simple class to get the image
+
     private String mTitle;
     private String mAuthor;
     private String mAuthorId;
     private String mLink;
     private String mTags;
     private String mImage;
-    private String mPublishedDate;
-    private String mDescription;
 
-    public Photo(String title, String author, String authorId,String link, String tags, String image, String publishedDate, String description) {
+    public Photo(String title, String author, String authorId, String link, String tags, String image) {
         mTitle = title;
         mAuthor = author;
         mAuthorId = authorId;
         mLink = link;
         mTags = tags;
         mImage = image;
-        mPublishedDate = publishedDate;
-        mDescription = description;
     }
 
-    public String getLink() {
-        return mLink;
-    }
-
-    public void setLink(String link) {
-        mLink = link;
-    }
-
-    public String getTitle() {
+    String getTitle() {
         return mTitle;
     }
 
-    public String getAuthor() {
+    String getAuthor() {
         return mAuthor;
     }
 
-    public String getAuthorId() {
+    String getAuthorId() {
         return mAuthorId;
     }
 
-    public String getTags() {
+    String getLink() {
+        return mLink;
+    }
+
+    String getTags() {
         return mTags;
     }
 
-    public String getImage() { return mImage; }
-
-    public String getPublishedDate() {
-        return mPublishedDate;
+    String getImage() {
+        return mImage;
     }
-
-    public String getDescription() {
-        return mDescription;
-    }
-
 
     @Override
     public String toString() {
@@ -63,10 +48,9 @@ class Photo {
                 "mTitle='" + mTitle + '\'' +
                 ", mAuthor='" + mAuthor + '\'' +
                 ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
                 ", mTags='" + mTags + '\'' +
                 ", mImage='" + mImage + '\'' +
-                ", mPublishedDate='" + mPublishedDate + '\'' +
-                ", mDescription='" + mDescription + '\'' +
                 '}';
     }
 }
