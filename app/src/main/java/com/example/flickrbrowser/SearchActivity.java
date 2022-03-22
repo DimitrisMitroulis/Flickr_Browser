ab3a37783cd413b51b9dc0f6ed32a8549372f8fe
DimitrisMitroulis <amfimitrul@gmail.com>
2022-03-22 23:38:23 +0200

    added 2 activities

--- app/src/main/java/com/example/flickrbrowser/SearchActivity.java
+++ app/src/main/java/com/example/flickrbrowser/SearchActivity.java
@@ -29,23 +29,6 @@ public class SearchActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_search);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_search);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
